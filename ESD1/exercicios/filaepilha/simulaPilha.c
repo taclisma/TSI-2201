612437987efb40f877f674dd424c548f6c962b65
mariana ferro <marianafe00@gmail.com>
2022-05-11 12:05:16 -0300

    Create simulaPilha.c

---
+++ ESD1/exercicios/filaepilha/simulaPilha.c
@@ -0,0 +1,64 @@
+/*
+Simular uma pilha
+1- inserir números de 1 a 7
+2- remover um número (a)
+3-remover outro número (b)
+4-Dividir a/b e empilhar o resultado
+Repetir 2,3 e 4 até restar apenas um elemento na pilha
+
+Mostrar passo-a-passo
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 3
+
+typedef struct{
+    int topo;
+    int conteudo[N]
+} pilha;
+
+void inserir(contato* ag){
+
+}
+
+void ver(contato* ag){
+
+}
+
+int main (){
+    ;
+
+    do{
+
+
+        switch (m)
+        {
+        case 1:
+
+        break;
+            
+        
+        case 2:
+
+        break;
+        
+        case 3:
+
+        break;
+        
+        default:
+            printf("\nescolha uma opção valida\n");
+        break;
+        }
+
+
+    } while (m != 3);
+
+}
+
+        /*sair
+            for(i = 0; i<c; i++){
+                free(agenda[i]);
+            }*/
