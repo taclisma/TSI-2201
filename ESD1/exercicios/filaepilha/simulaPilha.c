aa69e6de252787ecd72aa2f5775e36d9f73dd23b
mariana ferro <marianafe00@gmail.com>
2022-05-25 10:28:18 -0300

    Update simulaPilha.c

--- ESD1/exercicios/filaepilha/simulaPilha.c
+++ ESD1/exercicios/filaepilha/simulaPilha.c
@@ -5,60 +5,41 @@ Simular uma pilha
 3-remover outro número (b)
 4-Dividir a/b e empilhar o resultado
 Repetir 2,3 e 4 até restar apenas um elemento na pilha
-
 Mostrar passo-a-passo
 */
 
 #include <stdlib.h>
 #include <stdio.h>
-
 #define N 3
 
+/*/struct da pilha
 typedef struct{
-    int topo;
-    int conteudo[N]
-} pilha;
+	int *topo;
+	int num[N];
+} pilha;*/
 
-void inserir(contato* ag){
 
-}
-
-void ver(contato* ag){
-
+void push(int p[], int *t, int valor){
+	if(*t == N){
+		printf("pilha cheia");
+	} else {
+		(*t)++;
+		p[*t] = valor;
+	}
 }
 
 int main (){
-    ;
-
-    do{
-
-
-        switch (m)
-        {
-        case 1:
-
-        break;
-            
-        
-        case 2:
-
-        break;
-        
-        case 3:
-
-        break;
-        
-        default:
-            printf("\nescolha uma opção valida\n");
-        break;
-        }
-
-
-    } while (m != 3);
-
+	int topo = -1;
+	int p[N] = {0};
+	int valor = 1, i;
+	
+	printf("antes push %d\n", p[topo]);
+	
+	for(i = 0; i < N; i++){
+	    push(p, &topo, valor);
+	    valor++;
+	    printf("push %d\n", p[topo]);
+	}
+	
+return 0;
 }
-
-        /*sair
-            for(i = 0; i<c; i++){
-                free(agenda[i]);
-            }*/
